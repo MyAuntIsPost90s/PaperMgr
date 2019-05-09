385d5cd109e2b389e06dcd37d3cc2f6be69c8b01
MyAuntIsPost90s <1126670571@qq.com>
2019-05-09 11:57:12 +0800

    加入多环境配置

---
+++ PaperMgrService/profile.groovy
@@ -0,0 +1,11 @@
+environments {
+    local {   
+        jdbc {
+            ip = '127.0.0.1'
+            port = '3306'
+            db = 'lingshi_papermgr'
+            user = 'root'
+            pwd = 'root'
+        }
+    }
+}
